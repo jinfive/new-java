a58f9df900dfd2b10c5084cd823c4fc9517100c7
Kjo <krt8599@naver.com>
2024-10-02 23:07:37 +0900

    오늘 마지막 문제 커밋
    연휴다 잘 쉬다 오자

--- src/main/java/org/example/day5/array2/LastQ3.java
+++ src/main/java/org/example/day5/array2/LastQ3.java
@@ -1,5 +1,6 @@
 package org.example.day5.array2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LastQ3 {
@@ -9,27 +10,46 @@ public class LastQ3 {
         while문과 Scanner를 이용해서 최고 점수 및 평균 점수를 출력하는 코드를 작성해보세요.
          */
         Scanner sc = new Scanner(System.in);
+        int[] arr = null;
         while (true) {
 
             System.out.println("---------------------------------------------------------");
-            System.out.println("1.학생수\t\t2.점수입력\t\t3.점리스트\t\t4.분석\t\t5.종료");
+            System.out.println("1.학생수\t\t2.점수입력\t\t3.점수리스트\t\t4.분석\t\t5.종료");
             System.out.println("---------------------------------------------------------");
             System.out.print("선택>>");
             int n = sc.nextInt();
             switch (n) {
                 case 1:
-
+                    System.out.print("학생수 입력>>>");
+                    int a =sc.nextInt();
+                    arr = new int[a];
                     break;
                 case 2:
+                    for (int i = 0; i <arr.length ; i++) {
+                        System.out.println("arr["+i+"]>>");
+                        arr[i] = sc.nextInt();
+                    }
                     break;
                 case 3:
+                    for (int i = 0; i <arr.length ; i++) {
+                        System.out.println("arr["+i+"]"+arr[i]);
+                    }
                     break;
                 case 4:
+                    Arrays.sort(arr);
+                    System.out.println("최고점수>>"+ arr[arr.length-1]);
+                    System.out.println("최저점수>>"+ arr[0]);
+                    int sum = 0;
+                    for (int i = 0; i < arr.length ; i++) {
+                        sum += arr[i];
+                    }
+                    System.out.println("평균은"+(double)sum/arr.length);
                     break;
                 case 5:
+                    System.out.println("프로그램 종료");
                     System.exit(0);
                     break;
-            }
-        }
+            }//switch
+        }//while
     }
 }
