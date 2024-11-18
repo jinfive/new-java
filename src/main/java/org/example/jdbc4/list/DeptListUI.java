3ee3088fe4fe3f71e2df7329c0a13880a29c1016
Kjo <krt8599@naver.com>
2024-11-18 11:41:23 +0900

    리스트를 이용한 다중검색

--- src/main/java/org/example/jdbc4/list/DeptListUI.java
+++ src/main/java/org/example/jdbc4/list/DeptListUI.java
@@ -1,4 +1,22 @@
 package org.example.jdbc4.list;
 
+import org.example.jdbc4.one.DeptDAO;
+import org.example.jdbc4.one.DeptVo;
+
+import java.util.ArrayList;
+
 public class DeptListUI {
+    public static void main(String[] args) throws Exception {
+        DeptDAO dao = new DeptDAO();
+        //모든 리스트를 가지고 오자
+        ArrayList<DeptVo> list = dao.list();
+        //반복해서 꺼내서 출력~! {vo,vo}
+        for(DeptVo bag : list){
+            System.out.println("검색한 번호"+bag.getDeptno());
+            System.out.println("검색한 이름"+bag.getDname());
+            System.out.println("검색한 지역"+bag.getLoc());
+            System.out.println("-------------------");
+        }//for
+
+    }
 }
--- src/main/java/org/example/jdbc4/one/DeptDAO.java
+++ src/main/java/org/example/jdbc4/one/DeptDAO.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class DeptDAO {
 
     Connection con = null;
+
     //jdbc4단계
     public DeptDAO() throws Exception {
         //객체 생성시 1-2 단계를 실행하게 하자
@@ -20,6 +21,7 @@ public class DeptDAO {
         con = DriverManager.getConnection(url, id, pw);
         System.out.println("2. db연결 성공!");
     }
+
     //crud 기능별 메서드 추가 하면 끝.
     //각각의 메서드는 3~4단계만 넣어줌
     //read는 (slect 검색,읽기)
@@ -30,14 +32,14 @@ public class DeptDAO {
         DeptVo bag = new DeptVo();
 
         //3단계
-        String sql  ="Select * from dept where deptno = ?";
+        String sql = "Select * from dept where deptno = ?";
         PreparedStatement ps = con.prepareStatement(sql);//java는 sql을 모르기 때문에
         ps.setInt(1, deptno);
 
         //4단계
         //테이블 형태로 검색 결과가 와야함.
         ResultSet table = ps.executeQuery(); // 컬럼 + 값들(row)
-        if(table.next()){
+        if (table.next()) {
             //테이블에 값이 있으면 row에 있는 값들을 꺼내서 vo에 넣자.
             //ORM과정
             bag.setDeptno(table.getInt("deptno"));
@@ -47,13 +49,30 @@ public class DeptDAO {
         }
         return bag;
     }
-    public ArrayList<DeptVo> list(){
+
+    public ArrayList<DeptVo> list() throws SQLException {
         //row하나당 가방 1개 필요
         //row 가 5 개라면 가방 5개 필요
         //ui에 가방 5개 리턴해서 전달하려면 묶어 줘야함.
         //자바에서는 리턴을 할때, 무조건!! 하나로 묶어서 해야함.
         //가방을 묶어줄 용도로 사용하는것이 list 타입의 객체(ArrayList를 사용함)
         ArrayList<DeptVo> list = new ArrayList<>();
+        String sql = "Select * from dept";
+        PreparedStatement ps = con.prepareStatement(sql);//java는 sql을 모르기 때문에
+
+        //4단계
+        //테이블 형태로 검색 결과가 와야함.
+        ResultSet table = ps.executeQuery(); // 컬럼 + 값들(row)
+        while (table.next()) {
+            //테이블에 값이 있으면 row에 있는 값들을 꺼내서 vo에 넣자.
+            //ORM과정
+            DeptVo bag = new DeptVo();
+            bag.setDeptno(table.getInt("deptno"));
+            bag.setDname(table.getString("dname"));
+            bag.setLoc(table.getString("loc"));
+            list.add(bag);
+        }
         return list;
+
     }
 }
